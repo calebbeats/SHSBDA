23e25c985a4e8ba9a932b185fd99de83cb56c3de
chasemarley <cmarley@uco.edu>
2017-09-21 23:23:34 -0500

    Melee
    Added temporary sprite for melee attack, still working on getting the melee attack down. Right now it acts just like missle attack. Missle fires from right click and melee fires from left click.

--- src/controller/MouseController.java
+++ src/controller/MouseController.java
@@ -17,24 +17,24 @@ public class MouseController extends MouseAdapter {
         py = me.getY();
 
         Shooter shooter = (Shooter) Main.gameData.friendFigures.get(0);
-        
+
         if (me.getButton() == MouseEvent.BUTTON1) {//Left click detected, initiate melee attack
             //melee attack in the direction the mouse is in regards to the player
             Melee m = new Melee(
-                shooter.getXofMissileShoot(),
-                shooter.getYofMissileShoot(),
-                px, py // direction in which to melee attack
+                    shooter.getXofMissileShoot(),
+                    shooter.getYofMissileShoot(),
+                    px, py // direction in which to melee attack
             );
             Main.gameData.friendFigures.add(m);
         }
-        
-        if(me.getButton() == MouseEvent.BUTTON3){ //Right click detected, initiate ranged attack
+
+        if (me.getButton() == MouseEvent.BUTTON3) { //Right click detected, initiate ranged attack
             //shoot a missle at the mouse press location
-            Missile m = new Missile( 
-                shooter.getXofMissileShoot(),
-                shooter.getYofMissileShoot(),
-                px, py // target location where the missile explodes
-                );
+            Missile m = new Missile(
+                    shooter.getXofMissileShoot(),
+                    shooter.getYofMissileShoot(),
+                    px, py // target location where the missile explodes
+            );
             Main.gameData.friendFigures.add(m);
         }
 
--- src/model/Melee.java
+++ src/model/Melee.java
@@ -15,25 +15,28 @@ public class Melee extends GameFigure {
 
     // missile size
     private static final int SIZE = 10;
-    private static final int MAX_EXPLOSION_SIZE = 3;
+    private static final int MAX_EXPLOSION_SIZE = 6;
     private float dx; // displacement at each frame
     private float dy; // displacement at each frame
-    private int animationCheck=0;
+    private int animationCheck = 0;
 
     // public properties for quick access
     public Color color;
     public Point2D.Float target;
 
-    private static final int UNIT_TRAVEL_DISTANCE = 7; // per frame move
+    private static final int UNIT_TRAVEL_DISTANCE = 3; // per frame move
 
     private int explosionCounter = 0;
     public int distanceTraveled;
-    private Image launcherImage;
-    private Image launcherImage2;
-    private Image explosion1;
-    private Image explosion2;
-    private Image explosion3;
-    
+    // private Image launcherImage;
+    //private Image launcherImage2;
+    //private Image explosion1;
+    // private Image explosion2;
+    // private Image explosion3;
+    private Image swordUp;
+    private Image swordDown;
+    private Image swordRight;
+    private Image swordLeft;
 
     /**
      *
@@ -45,14 +48,17 @@ public class Melee extends GameFigure {
      */
     public Melee(float sx, float sy, float tx, float ty) {
         super(sx, sy);
-        
+
         this.target = new Point2D.Float(tx, ty);
         this.color = color;
+        
+        //dx = tx;
+        //dy = ty;
         
         double angle = Math.atan2(Math.abs(ty - sy), Math.abs(tx - sx));
         dx = (float) (UNIT_TRAVEL_DISTANCE * Math.cos(angle));
         dy = (float) (UNIT_TRAVEL_DISTANCE * Math.sin(angle));
-        
+
         if (tx > sx && ty < sy) { // target is upper-right side
             dy = -dy; // dx > 0, dx < 0
         } else if (tx < sx && ty < sy) { // target is upper-left side
@@ -64,84 +70,98 @@ public class Melee extends GameFigure {
             // dx > 0 , dy > 0
         }
         
-        launcherImage = null;
-        
+
+        //launcherImage = null;
+
         try {
-           
-            launcherImage = ImageIO.read(getClass().getResource("fireball.gif"));
-            launcherImage2 = ImageIO.read(getClass().getResource("fireball2.png"));
-            explosion1 = ImageIO.read(getClass().getResource("explosion0.png"));
-            explosion2 = ImageIO.read(getClass().getResource("explosion1.png"));
-            explosion3 = ImageIO.read(getClass().getResource("explosion2.png"));
+
+            //launcherImage = ImageIO.read(getClass().getResource("fireball.gif"));
+            //launcherImage2 = ImageIO.read(getClass().getResource("fireball2.png"));
+            //explosion1 = ImageIO.read(getClass().getResource("explosion0.png"));
+            //explosion2 = ImageIO.read(getClass().getResource("explosion1.png"));
+            //explosion3 = ImageIO.read(getClass().getResource("explosion2.png"));
+            swordUp = ImageIO.read(getClass().getResource("swordUp.png"));
+            swordDown = ImageIO.read(getClass().getResource("swordDown.png"));
+            swordRight = ImageIO.read(getClass().getResource("swordRight.png"));
+            swordLeft = ImageIO.read(getClass().getResource("swordLeft.png"));
         } catch (IOException ex) {
-            JOptionPane.showMessageDialog(null, "Error: Cannot open shooter.png");
-           System.exit(-1);
+            JOptionPane.showMessageDialog(null, "Error: Cannot open image.");
+            System.exit(-1);
         }
-        
+
     }
-    
-    
+
     @Override
     public void render(Graphics2D g) {
-        if(state == STATE_ALIVE)
-        {
-            if(animationCheck == 0){
-                g.drawImage(launcherImage, (int)super.x, (int)super.y, 
-                30, 30, null);
-                animationCheck = 1;
-            }
-            else{
-                g.drawImage(launcherImage2, (int)super.x, (int)super.y, 
-                30, 30, null);
-                animationCheck = 0;
-            }
+        if (state == STATE_ALIVE) {
+            //if (animationCheck == 0) {
+                //g.drawImage(launcherImage, (int) super.x, (int) super.y,
+                //       30, 30, null);
+                g.drawImage(swordUp, (int) super.x, (int) super.y,
+                        30, 30, null);
+             //   animationCheck = 1;
+            //} else {
+                //g.drawImage(launcherImage2, (int) super.x, (int) super.y,
+                // 30, 30, null);
+              //  g.drawImage(swordUp, (int) super.x, (int) super.y,
+                    //    30, 30, null);
+             //   animationCheck = 0;
+           // }
         }
-        if(state == STATE_DYING){
-            if(explosionCounter ==0)
-            {
-                 g.drawImage(explosion1, (int)super.x, (int)super.y, 
-                30, 30, null);
+        if (state == STATE_DYING) {
+            if (explosionCounter == 0) {
+                g.drawImage(swordRight, (int) super.x, (int) super.y,
+                        30, 30, null);
             }
-            if(explosionCounter ==1)
-            {
-                g.drawImage(explosion2, (int)super.x, (int)super.y, 
-                30, 30, null);
+            if (explosionCounter == 1) {
+                g.drawImage(swordUp, (int) super.x, (int) super.y,
+                        30, 30, null);
             }
-            if(explosionCounter ==2)
-            {
-                g.drawImage(explosion3, (int)super.x, (int)super.y, 
-                30, 30, null);
+            if (explosionCounter == 2) {
+                g.drawImage(swordLeft, (int) super.x, (int) super.y,
+                        30, 30, null);
             }
-         
+            if (explosionCounter == 3) {
+                g.drawImage(swordUp, (int) super.x, (int) super.y,
+                        30, 30, null);
+            }
+            if (explosionCounter == 4) {
+                g.drawImage(swordRight, (int) super.x, (int) super.y,
+                        30, 30, null);
+            }
+            if (explosionCounter == 5) {
+                g.drawImage(swordDown, (int) super.x, (int) super.y,
+                        30, 30, null);
+            }
+
         }
     }
 
     @Override
     public void update() {
         updateState();
-        if(this.x > 470 || this.x < 20 || this.y >540 || this.y < 20)
-        {
-            this.goNextState();
-        }
+        //if (this.x > 470 || this.x < 20 || this.y > 540 || this.y < 20) {
+        //    this.goNextState();
+        //}
         if (state == STATE_ALIVE) {
             updateLocation();
-           distanceTraveled++;
+            distanceTraveled++;
         } else if (state == STATE_DYING) {
             updateSize();
         }
-        
+
     }
 
-    public void updateLocation() {    
+    public void updateLocation() {
         //when button clicked this happens
         super.x += dx;
         super.y += dy;
-        
+
     }
 
     public void updateSize() {
         explosionCounter++;
-         
+
     }
 
     public void updateState() {
@@ -160,7 +180,7 @@ public class Melee extends GameFigure {
 
     @Override
     public Rectangle2D getCollisionBox() {
-        return new Rectangle2D.Double(this.x - SIZE , this.y - SIZE, SIZE * 0.9D, SIZE * 0.9D);
+        return new Rectangle2D.Double(this.x - SIZE, this.y - SIZE, SIZE * 0.9D, SIZE * 0.9D);
     }
 
     @Override
