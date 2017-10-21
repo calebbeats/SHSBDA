b8f6217cb0299bc11a3049a093dd38383bdab423
Haseeba09 <haseeba09@gmail.com>
2017-10-20 20:21:16 -0500

    ice terrain exists; missles pass

--- src/model/GameData.java
+++ src/model/GameData.java
@@ -59,7 +59,7 @@ public class GameData {
             enemyFigures.add(new MeleeEnemy((int) (Math.random() * 500), (int) Math.random() * 200));
             enemyFigures.add(new SlowMage((int) (Math.random() * 500), (int) Math.random() * 200));
             enemyFigures.add(new SuicideEnemy((int) (Math.random() * 500), (int) Math.random() * 200));
-            terrainFigures.add(new BlockTerrain(200, 200));
+            terrainFigures.add(new IceTerrain(200, 200, 80, 80));
         }
     }
 
