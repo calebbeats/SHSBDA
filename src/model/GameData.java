d7192cf8709598142a4eb67cf286beff718aca00
calebbeats <calebmills1@gmail.com>
2017-11-19 00:37:35 -0600

    asd

--- src/model/GameData.java
+++ src/model/GameData.java
@@ -149,11 +149,9 @@ public class GameData {
             enemyFigures.add(new SlowMage((int) (Math.random() * 500), (int) (Math.random() * 200)));
         } else if (level == 10) {
             shooter.setXY(300, Main.WIN_HEIGHT);
-<<<<<<< HEAD
+
             friendFigures.add(shooter); 
-=======
-            friendFigures.add(shooter);
->>>>>>> Dev
+
             terrainFigures.add(new IceTerrain(50, 50, 450, 450));
             enemyFigures.add(new SlowMage((int) (0), (int) (100)));
             enemyFigures.add(new BlinkMage((int) (100), (int) (0)));
